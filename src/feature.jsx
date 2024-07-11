aaf5e3b673dc9b1970b0c7b713b4b58c04268afd
yash <yashtilala412@gmail.com>
2024-07-11 19:50:02 +0530

    favorite add

--- src/feature.jsx
+++ src/feature.jsx
@@ -1,28 +1,31 @@
 import React, { useState } from 'react';
 
 import React, { useEffect, useState } from 'react';
-const VideoList = () => {
-    const [videos, setVideos] = useState([
-        // Initial videos
-    ]);
+const [favorites, setFavorites] = useState([]);
 
-    const handleUpload = (event) => {
-        const newVideo = URL.createObjectURL(event.target.files[0]);
-        setVideos([...videos, { url: newVideo, title: 'New Video', description: 'New Description' }]);
-    };
-
-    return (
-        <div className="video-list">
-            <input type="file" onChange={handleUpload} />
-            {videos.map((video, index) => (
-                <div key={index} className="video">
-                    <h3>{video.title}</h3>
-                    <iframe width="300" height="200" src={video.url} title="YouTube Video" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-                </div>
-            ))}
-        </div>
-    );
+const handleFavorite = (index) => {
+    setFavorites([...favorites, videos[index]]);
 };
+
+return (
+    <div className="video-list">
+        {videos.map((video, index) => (
+            <div key={index} className="video">
+                <h3>{video.title}</h3>
+                <iframe width="300" height="200" src={video.url} title="YouTube Video" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                <button onClick={() => handleFavorite(index)}>Add to Favorites</button>
+            </div>
+        ))}
+        <h2>Favorites</h2>
+        {favorites.map((video, index) => (
+            <div key={index} className="video">
+                <h3>{video.title}</h3>
+                <iframe width="300" height="200" src={video.url} title="YouTube Video" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+            </div>
+        ))}
+    </div>
+);
+
 
 
 
