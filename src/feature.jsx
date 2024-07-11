a3393ca23b00d731c5da038e102d50a145f71165
yash <yashtilala412@gmail.com>
2024-07-11 19:48:49 +0530

    video duration

--- src/feature.jsx
+++ src/feature.jsx
@@ -1,45 +1,34 @@
 import React, { useState } from 'react';
 
-const [currentPage, setCurrentPage] = useState(1);
-const videosPerPage = 5;
-const indexOfLastVideo = currentPage * videosPerPage;
-const indexOfFirstVideo = indexOfLastVideo - videosPerPage;
-const currentVideos = videos.slice(indexOfFirstVideo, indexOfLastVideo);
+import React, { useEffect, useState } from 'react';
 
-return (
-    <div className="video-list">
-        {currentVideos.map((video, index) => (
-            <div key={index} className="video">
-                <h3>{video.title}</h3>
-                <iframe width="300" height="200" src={video.url} title="YouTube Video" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-            </div>
-        ))}
-        <div className="pagination">
-            <button onClick={() => setCurrentPage(currentPage - 1)} disabled={currentPage === 1}>Previous</button>
-            <button onClick={() => setCurrentPage(currentPage + 1)} disabled={indexOfLastVideo >= videos.length}>Next</button>
+const VideoList = () => {
+    const [durations, setDurations] = useState({});
+
+    useEffect(() => {
+        videos.forEach((video, index) => {
+            fetch(`https://www.googleapis.com/youtube/v3/videos?id=${video.id}&part=contentDetails&key=YOUR_API_KEY`)
+                .then(response => response.json())
+                .then(data => {
+                    const duration = data.items[0].contentDetails.duration;
+                    setDurations(prev => ({ ...prev, [index]: duration }));
+                });
+        });
+    }, []);
+
+    return (
+        <div className="video-list">
+            {videos.map((video, index) => (
+                <div key={index} className="video">
+                    <h3>{video.title}</h3>
+                    <p>Duration: {durations[index]}</p>
+                    <iframe width="300" height="200" src={video.url} title="YouTube Video" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                </div>
+            ))}
         </div>
-        .video-list {
-    display: flex;
-    flex-wrap: wrap;
-}
+    );
+};
 
-.video {
-    flex: 1 1 calc(33.333% - 10px);
-    margin: 5px;
-}
-
-@media (max-width: 768px) {
-    .video {
-        flex: 1 1 calc(50% - 10px);
-    }
-}
-
-@media (max-width: 480px) {
-    .video {
-        flex: 1 1 100%;
-    }
-}
-    </div>
 );
 
 
