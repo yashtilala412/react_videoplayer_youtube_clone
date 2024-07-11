7ab448d0ad61416f6c22af2831da01a49038ff5c
yash <yashtilala412@gmail.com>
2024-07-11 19:51:13 +0530

    video rating

--- src/feature.jsx
+++ src/feature.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 
 import React, { useEffect, useState } from 'react';
-const handleShare = (url) => {
-    navigator.clipboard.writeText(url);
-    alert("Video URL copied to clipboard!");
+const [ratings, setRatings] = useState({});
+
+const handleRating = (index, rating) => {
+    setRatings({ ...ratings, [index]: rating });
 };
 
 return (
@@ -12,11 +13,28 @@ return (
             <div key={index} className="video">
                 <h3>{video.title}</h3>
                 <iframe width="300" height="200" src={video.url} title="YouTube Video" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-                <button onClick={() => handleShare(video.url)}>Share</button>
+                <div>
+                    <label>
+                        <input type="radio" name={`rating-${index}`} value="1" onClick={() => handleRating(index, 1)} /> 1
+                    </label>
+                    <label>
+                        <input type="radio" name={`rating-${index}`} value="2" onClick={() => handleRating(index, 2)} /> 2
+                    </label>
+                    <label>
+                        <input type="radio" name={`rating-${index}`} value="3" onClick={() => handleRating(index, 3)} /> 3
+                    </label>
+                    <label>
+                        <input type="radio" name={`rating-${index}`} value="4" onClick={() => handleRating(index, 4)} /> 4
+                    </label>
+                    <label>
+                        <input type="radio" name={`rating-${index}`} value="5" onClick={() => handleRating(index, 5)} /> 5
+                    </label>
+                </div>
             </div>
         ))}
     </div>
 );
+
 
 
 
