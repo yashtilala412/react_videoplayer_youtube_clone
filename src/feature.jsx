9c80bc9e50a1da62b5f6715f2a0e8884848e2ba0
yash <yashtilala412@gmail.com>
2024-07-11 19:45:32 +0530

    comments add

--- src/feature.jsx
+++ src/feature.jsx
@@ -1,15 +1,13 @@
 import React, { useState } from 'react';
 
 const VideoList = () => {
-    const [likes, setLikes] = useState({});
-    const [dislikes, setDislikes] = useState({});
+    const [comments, setComments] = useState({});
 
-    const handleLike = (index) => {
-        setLikes({ ...likes, [index]: (likes[index] || 0) + 1 });
-    };
-
-    const handleDislike = (index) => {
-        setDislikes({ ...dislikes, [index]: (dislikes[index] || 0) + 1 });
+    const handleComment = (index, comment) => {
+        setComments({
+            ...comments,
+            [index]: [...(comments[index] || []), comment],
+        });
     };
 
     return (
@@ -19,13 +17,24 @@ const VideoList = () => {
                     <h3>{video.title}</h3>
                     <iframe width="300" height="200" src={video.url} title="YouTube Video" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
                     <div>
-                        <button onClick={() => handleLike(index)}>Like ({likes[index] || 0})</button>
-                        <button onClick={() => handleDislike(index)}>Dislike ({dislikes[index] || 0})</button>
+                        <input type="text" placeholder="Add a comment" onKeyDown={(e) => {
+                            if (e.key === 'Enter') {
+                                handleComment(index, e.target.value);
+                                e.target.value = '';
+                            }
+                        }} />
+                        <ul>
+                            {(comments[index] || []).map((comment, i) => (
+                                <li key={i}>{comment}</li>
+                            ))}
+                        </ul>
                     </div>
                 </div>
             ))}
         </div>
     );
+};
+
 };
 
 
