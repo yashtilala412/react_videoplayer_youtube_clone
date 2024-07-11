7dad98e361ebd3730e4889a7f710389d8b9d98fa
yash <yashtilala412@gmail.com>
2024-07-11 19:50:39 +0530

    share button add

--- src/feature.jsx
+++ src/feature.jsx
@@ -1,10 +1,9 @@
 import React, { useState } from 'react';
 
 import React, { useEffect, useState } from 'react';
-const [favorites, setFavorites] = useState([]);
-
-const handleFavorite = (index) => {
-    setFavorites([...favorites, videos[index]]);
+const handleShare = (url) => {
+    navigator.clipboard.writeText(url);
+    alert("Video URL copied to clipboard!");
 };
 
 return (
@@ -13,18 +12,12 @@ return (
             <div key={index} className="video">
                 <h3>{video.title}</h3>
                 <iframe width="300" height="200" src={video.url} title="YouTube Video" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-                <button onClick={() => handleFavorite(index)}>Add to Favorites</button>
-            </div>
-        ))}
-        <h2>Favorites</h2>
-        {favorites.map((video, index) => (
-            <div key={index} className="video">
-                <h3>{video.title}</h3>
-                <iframe width="300" height="200" src={video.url} title="YouTube Video" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                <button onClick={() => handleShare(video.url)}>Share</button>
             </div>
         ))}
     </div>
 );
+
 
 
 
