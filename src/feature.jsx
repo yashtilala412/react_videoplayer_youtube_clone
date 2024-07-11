a0f7880070a17c4dd3f33e4e4410da04c4a5d86b
yash <yashtilala412@gmail.com>
2024-07-11 19:44:47 +0530

    likes dislikes adds

--- src/feature.jsx
+++ src/feature.jsx
@@ -1,21 +1,33 @@
 import React, { useState } from 'react';
 
-const videos = [
-    { url: "https://youtu.be/OzI9M74IfR0?si=N9Pjj_C0k4ZLi_5g", title: "Video 1", description: "Description 1" },
-    // Add more videos
-];
+const VideoList = () => {
+    const [likes, setLikes] = useState({});
+    const [dislikes, setDislikes] = useState({});
 
-return (
-    <div className="video-list">
-        {videos.map((video, index) => (
-            <div key={index} className="video">
-                <h3>{video.title}</h3>
-                <p>{video.description}</p>
-                <iframe width="300" height="200" src={video.url} title="YouTube Video" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-            </div>
-        ))}
-    </div>
-);
+    const handleLike = (index) => {
+        setLikes({ ...likes, [index]: (likes[index] || 0) + 1 });
+    };
+
+    const handleDislike = (index) => {
+        setDislikes({ ...dislikes, [index]: (dislikes[index] || 0) + 1 });
+    };
+
+    return (
+        <div className="video-list">
+            {videos.map((video, index) => (
+                <div key={index} className="video">
+                    <h3>{video.title}</h3>
+                    <iframe width="300" height="200" src={video.url} title="YouTube Video" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                    <div>
+                        <button onClick={() => handleLike(index)}>Like ({likes[index] || 0})</button>
+                        <button onClick={() => handleDislike(index)}>Dislike ({dislikes[index] || 0})</button>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+};
+
 
     
     
