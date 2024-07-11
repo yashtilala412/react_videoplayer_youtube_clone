5ad4e8b942cb71b3a7f043f32c68a1e4aec75779
yash <yashtilala412@gmail.com>
2024-07-11 20:00:34 +0530

    reply and comments

--- src/feature.jsx
+++ src/feature.jsx
@@ -24,6 +24,29 @@ return (
         ))}
     </div>
 );
+const VideoList = () => {
+    const [comments, setComments] = useState({});
+
+    const handleComment = (index, comment) => {
+        setComments({
+            ...comments,
+            [index]: [...(comments[index] || []), { text: comment, replies: [] }],
+        });
+    };
+
+    const handleReply = (videoIndex, commentIndex, reply) => {
+        const newComments = { ...comments };
+        newComments[videoIndex][commentIndex].replies.push(reply);
+        setComments(newComments);
+    };
+
+    return (
+        <div className="video-list">
+            {videos.map((video, index) => (
+                <div key={index} className="video">
+                    <h3>{video.title}</h3>
+                    <iframe width="300" height="200" src={video.url} title="YouTube Video" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                   
 
 );
 
