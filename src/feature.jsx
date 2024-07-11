06ce32821562ad524252c2dff504ff0272e2bf45
yash <yashtilala412@gmail.com>
2024-07-11 19:55:20 +0530

    related video

--- src/feature.jsx
+++ src/feature.jsx
@@ -1,21 +1,30 @@
 import React, { useState } from 'react';
 
 import React, { useEffect, useState } from 'react';
-const [currentVideo, setCurrentVideo] = useState(0);
-
-useEffect(() => {
-    const nextVideo = (currentVideo + 1) % videos.length;
-    const timer = setTimeout(() => setCurrentVideo(nextVideo), 30000); // 30 seconds for demo
-
-    return () => clearTimeout(timer);
-}, [currentVideo]);
+const relatedVideos = [
+    // List of related videos
+];
 
 return (
     <div className="video-list">
-        <h3>{videos[currentVideo].title}</h3>
-        <iframe width="400" height="600" src={videos[currentVideo].url} title="YouTube Video" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+        {videos.map((video, index) => (
+            <div key={index} className="video">
+                <h3>{video.title}</h3>
+                <iframe width="300" height="200" src={video.url} title="YouTube Video" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                <h4>Related Videos</h4>
+                <div className="related-videos">
+                    {relatedVideos.map((relatedVideo, i) => (
+                        <div key={i} className="video">
+                            <h5>{relatedVideo.title}</h5>
+                            <iframe width="150" height="100" src={relatedVideo.url} title="Related YouTube Video" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        ))}
     </div>
 );
+
 
 
 
